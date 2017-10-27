1ee93196f1c9b0ec3302b71293919a37061f3f4c
TankTian <chd.tianming@gmail.com>
2017-10-27 10:05:43 +0800

    delete the comment.

--- src/main/java/com/huaweiair/order/dao/OrderDbAdapter.java
+++ src/main/java/com/huaweiair/order/dao/OrderDbAdapter.java
@@ -22,23 +22,16 @@ public interface OrderDbAdapter {
 
   default List<Object> populate(ResultSet rs, Class<?> clazz)
       throws SQLException, InstantiationException, IllegalAccessException {
-    // 结果集的元素对象
     ResultSetMetaData rsmd = rs.getMetaData();
-    // 获取结果集的元素个数
     int colCount = rsmd.getColumnCount();
-    // 返回结果的列表集合
     List<Object> list = new ArrayList<Object>();
-    // 业务对象的属性数组
     Field[] fields = clazz.getDeclaredFields();
-    while (rs.next()) {// 对每一条记录进行操作
-      Object obj = clazz.newInstance();// 构造业务对象实体
-      // 将每一个字段取出进行赋值
+    while (rs.next()) {
+      Object obj = clazz.newInstance();
       for (int i = 1; i <= colCount; i++) {
         Object value = rs.getObject(i);
-        // 寻找该列对应的对象属性
         for (int j = 0; j < fields.length; j++) {
           Field f = fields[j];
-          // 如果匹配进行赋值
           if (f.getName().equalsIgnoreCase(rsmd.getColumnName(i))) {
             boolean flag = f.isAccessible();
             f.setAccessible(true);
